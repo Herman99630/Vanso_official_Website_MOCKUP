752d7d4315fec4e088996abe1ba3c0853c8ab4ff
Herman99630 <hsun0146@uni.sydney.edu.au>
2026-09-14 16:00:48 +0800

    content: restore official business email

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -8,7 +8,7 @@
         </RouterLink>
         <div class="socials"><a href="https://discord.gg/GK8nNyvzFf">Discord</a><a href="https://www.tiktok.com/@vansomusic">TikTok</a><a href="https://www.instagram.com/vansomusic/">Instagram</a></div>
       </div>
-      <div><strong>Contact</strong><a href="mailto:service@vansound.com">service@vansound.com</a></div>
+      <div><strong>Contact</strong><a href="mailto:service@vansound.com">service@vansound.com</a><a href="mailto:office2025@vansound.com">Business: office2025@vansound.com</a></div>
       <div class="legal"><span><a href="#privacy">Privacy Policy</a><a href="#terms">User Agreement</a><a href="#community">Community Guidelines</a><a href="#copyright">Copyright Policy</a></span><small>© 2026 Vanso. All rights reserved.</small></div>
     </div>
   </footer>
